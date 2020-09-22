a91efcbb6c2de8f444718cef672b95a3bc34882d
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-09-22 20:42:30 +0530

    Done with basic app functionality

--- Grocery List App/Grocery List App/ViewController.swift
+++ Grocery List App/Grocery List App/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var groceryTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textField.becomeFirstResponder()
+        
+        groceryTextView.delegate = self
     }
 
-
+    @IBAction func addItem(_ sender: Any) {
+        
+        if let text = textField.text, text != "" {
+            groceryTextView.text.append("\(text)\n")
+            
+            textField.text = ""
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textField.resignFirstResponder()
+    }
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
 }
 
