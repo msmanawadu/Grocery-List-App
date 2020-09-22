536870dd18e17cac9adac2ed7b5fd9f8bdcad8f9
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-09-22 16:57:50 +0530

    Initial Commit

---
+++ Grocery List App/Grocery List App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Grocery List App
+//
+//  Created by Sandeepa Manawadu on 2020/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
